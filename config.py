966f06541bb2038f342420a3da2b77e8910730fe
trojan <andrew.drish@yandex.ru>
2023-02-11 14:14:39 +0300

    helpplease

--- config.py
+++ config.py
@@ -1,10 +1,6 @@
-# Это файл конфигурации приложения, здесь может хранится путь к бд, ключ шифрования, что-то еще.
-# Чтобы добавить новую настройку, допишите ее в класс.
+class Config(object):
+    DEBUG = True
+    SECRET_KEY = 'TEST'
+    SQLALCHEMY_DATABASE_URI = "sqlite:///movies.db"
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
-# Пример
-
-# class Config(object):
-#     DEBUG = True
-#     SECRET_HERE = '249y823r9v8238r9u'
-#     SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-#     SQLALCHEMY_TRACK_MODIFICATIONS = False
